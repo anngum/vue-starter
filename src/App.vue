8dc81b87c0a34e2a44222f8cdccc4a4694606f6a
student2 <student2@pcoip.ki.agh.edu.pl>
2022-05-28 10:12:02 +0200

    alert

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
     e-mail: <input type="email" v-model="email">
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
     <br>
     password:<input type="password" v-model="pasword">
-    <h1>Twój e-mail to {{ email }}</h1>
+
+    <!--  <h1>Twój e-mail to {{ email }}</h1> -->
+
+
   </div>
 </template>
 
@@ -16,6 +23,11 @@ export default {
       email: '',
       password: ''
     };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
   }
 }
 </script>
